40188b4b36277fc6c3cfc00d96686d2ba9417804
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-29 22:45:43 +0100

    Create About component with styles

---
+++ src/components/views/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from '../'
+import '../../styles/About.css'
+import { Link } from 'react-router-dom';
+
+
+const About = () => {
+
+    return (
+        <section className="About">
+            <h2 className="About__heading">Below there are few tips how to use our tool: </h2>
+            <ol>
+                <li>
+                    <p>
+                        If you want to create a new category:
+                    </p>
+                    {/* <p>go to <Link to="/">??</Link>! and press <b>Create category button</b></p> */}
+                </li>
+                <li>
+                    <p>
+                        If you want to check the current categories:
+                    </p>
+                    <p>go to <b>Your Categories</b></p>
+                </li>
+                <li>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, iure.
+                    </p>
+                    <p>go to <b>Your Categories</b></p>
+                </li>
+                <li>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, iure.
+                    </p>
+                    <p>go to <b>Your Categories</b></p>
+                </li>
+            </ol>
+        </section>
+
+    )
+}
+
+export default About
