538e9c1ad9b0a34d69f4968c1a8a555ffbeaeced
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-29 17:44:47 +0100

    Create Header component with styles

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Header.css'
+
+const Header = () => {
+
+    return (
+        <header className="Header__header">
+            <h1 className="Header__heading">Flashboards creator </h1>
+        </header>
+    )
+}
+
+export default Header
