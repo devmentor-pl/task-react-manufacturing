afd6b7d8396e073d7c58ab312c1a39e674c987d6
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-29 22:47:03 +0100

    Change h1 to h2 in Home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -6,8 +6,10 @@ const Home = () => {
 
     return (
         <section className="Home__header">
-            <h1 className="Home__heading">Create your own flashcards and start learning!</h1>
-            <Button>Create new category</Button>
+            <h2 className="Home__heading">Create your own flashcards and start learning!</h2>
+            <div>
+                <Button>Create new category</Button>
+            </div>
         </section>
 
     )
