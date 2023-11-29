e055ba12205a09745a8703ec9992a0d76ff9dca3
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-29 17:44:17 +0100

    Create Home component with styles

---
+++ src/components/views/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../styles/Home.css'
+
+const Home = () => {
+
+    return (
+        <section className="Home__header">
+            <h1 className="Home__heading">Create your own flashcards and learn!</h1>
+        </section>
+
+    )
+}
+
+export default Home
