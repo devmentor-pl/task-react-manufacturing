d70a32186d994acad3a05df97f8b8db5b41a2e05
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-30 12:36:54 +0100

    Create flashcards.types

---
+++ src/modules/flashcards/flashcards.types.js
@@ -0,0 +1,11 @@
+const ACTIVE_CATEGORYFORM = 'setActiveCategoryForm'
+const INACTIVE_CATEGORYFORM = 'setInactiveCategoryForm'
+const ADD_CATEGORY = 'addCategory'
+const DELETE_CATEGORY = 'deleteCategory'
+
+export default {
+    ACTIVE_CATEGORYFORM,
+    INACTIVE_CATEGORYFORM,
+    ADD_CATEGORY,
+    DELETE_CATEGORY
+}
