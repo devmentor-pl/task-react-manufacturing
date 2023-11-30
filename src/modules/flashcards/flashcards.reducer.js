7da29f2bbd9e712464760a2eb4fdd57149908add
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-30 13:18:01 +0100

    Create reducer

---
+++ src/modules/flashcards/flashcards.reducer.js
@@ -0,0 +1,22 @@
+import { types } from '.'
+
+const initState = { categoryFormIsActive: false }
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case types.ACTIVE_CATEGORYFORM:
+            return {
+                ...state,
+                categoryFormIsActive: true
+            }
+        case types.INACTIVE_CATEGORYFORM:
+            return {
+                ...state,
+                categoryFormIsActive: false
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
