ceb447ff076acedaa8600fe682718077ab7c9e24
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-01 14:21:58 +0100

    Add types to flashcards.types

--- src/modules/flashcards/flashcards.types.js
+++ src/modules/flashcards/flashcards.types.js
@@ -3,10 +3,14 @@ const ACTIVE_CATEGORYFORM = 'setActiveCategoryForm'
 const INACTIVE_CATEGORYFORM = 'setInactiveCategoryForm'
 const ADD_CATEGORY = 'addCategory'
 const DELETE_CATEGORY = 'deleteCategory'
+const ADD_FLASHCARD = 'addFlashcard'
+const DELETE_FLASHCARD = 'deleteFlashcard'
 
 export default {
     ACTIVE_CATEGORYFORM,
     INACTIVE_CATEGORYFORM,
     ADD_CATEGORY,
-    DELETE_CATEGORY
+    DELETE_CATEGORY,
+    ADD_FLASHCARD,
+    DELETE_FLASHCARD
 }
